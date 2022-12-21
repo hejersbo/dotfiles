175f99b81a5187b2aa5c29ecf184acf3441d35a0
Henrik Ejersbo <hejersbo@ece.au.dk>
2022-12-21 11:53:31 +0100

    Add missing map file

---
+++ nvim/lua/hejersbo/lsp_on_attach.lua
@@ -0,0 +1,39 @@
+local function lsp_on_attach(client, bufnr)
+  -- We create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gD', vim.lsp.buf.definition, '[G]oto [D]eclaration')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Diagnostics
+  nmap('[d', vim.diagnostic.goto_prev, 'Diagnostic Next')
+  nmap(']d', vim.diagnostic.goto_next, 'Diagnostic Prev')
+  nmap('<leader>d', vim.diagnostic.open_float, 'Diagnostic Open')
+  nmap('<leader>q', vim.diagnostic.setloclist, 'Diagnostic Add Location')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
+
+return lsp_on_attach
