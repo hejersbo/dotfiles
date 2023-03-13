14f6ea3607e0bd96369ae0067530c8e84913a124
Henrik Ejersbo <hejersbo@ece.au.dk>
2023-03-13 20:09:23 +0100

    Add zenbones

--- nvim/lua/hejersbo/packer.lua
+++ nvim/lua/hejersbo/packer.lua
@@ -85,6 +85,15 @@ require('packer').startup(function(use)
       }
     end
   }
+
+  use {
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    requires = "rktjmp/lush.nvim"
+  }
+
   use 'nvim-lualine/lualine.nvim'
 
   use 'arcticicestudio/nord-vim'
